d8dd211ebb8e0782eba9d72832e735e8f2098265
Quyet1273 <vnbiplo@gmail.com>
2026-03-02 14:28:51 +0700

    Backend: update CORS allowedOrigins array

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,26 +13,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // ================= CORS =================
-// ================= CORS =================
 const allowedOrigins = [
+  
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:5173",
   "https://hotelmanagement-ecru.vercel.app", //link Vercel 
 ]
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Cho phép các request không có origin (như Postman hoặc mobile app)
-      if (!origin) return callback(null, true);
-      
-      if (allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV === 'development') {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins, 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -41,17 +31,21 @@ app.use(
 // app.use(
 //   cors({
 //     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true)
-
-//       if (allowedOrigins.includes(origin)) {
-//         callback(null, true)
+//       // Cho phép các request không có origin (như Postman hoặc mobile app)
+//       if (!origin) return callback(null, true);
+      
+//       if (allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV === 'development') {
+//         callback(null, true);
 //       } else {
-//         callback(new Error("Not allowed by CORS"))
+//         callback(new Error("Not allowed by CORS"));
 //       }
 //     },
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
 //     credentials: true,
 //   })
 // )
+
 
 // ================= SUPABASE TEST =================
 const testSupabase = async () => {
